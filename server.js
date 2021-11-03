bec922a7d79c713503dcfb8bc23d587d5b9f9335
Vlad <vladzbr1@gmail.com>
2021-11-03 19:30:12 -0400

    Initial setup for backend

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const app = express();
+
+app.use(cors({ credentials: true }))
+app.use(express.json())
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello Gilbert and Vlad');
+})
+
+app.listen(PORT, () =>  {
+    console.log(`I'm listening`);
+})
+
+mongoose.connect(process.env.MONGODB_URI, () => {
+    console.log('connected to mongod');
+});
